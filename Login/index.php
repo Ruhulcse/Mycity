1fcbb6974903011f5c7aa402d9b39e2164e7ba15
Ruhul amin <ruhul.cse7862@gmail.com>
2019-02-22 19:27:28 +0600

    login interface

---
+++ Login/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+  <script type="text/javascript" src="script.js"></script>
+  <div class="panda">
+  <div class="ear"></div>
+  <div class="face">
+    <div class="eye-shade"></div>
+    <div class="eye-white">
+      <div class="eye-ball"></div>
+    </div>
+    <div class="eye-shade rgt"></div>
+    <div class="eye-white rgt">
+      <div class="eye-ball"></div>
+    </div>
+    <div class="nose"></div>
+    <div class="mouth"></div>
+  </div>
+  <div class="body"> </div>
+  <div class="foot">
+    <div class="finger"></div>
+  </div>
+  <div class="foot rgt">
+    <div class="finger"></div>
+  </div>
+</div>
+<form action="includes/login.inc.php" method="post">
+    <script type="text/javascript" src="script.js"></script>
+  <div class="hand"></div>
+  <div class="hand rgt"></div>
+  <h1>MyCity Login</h1>
+  <div class="form-group">
+    <input class="form-control" type="text" name="mailud" placeholder="username/email">
+  </div>
+  <div class="form-group">
+     <input id="password" class="form-control" type="password" name="pwd" placeholder="password">
+     <script type="text/javascript" src="script.js"></script>
+    <p class="alert">Invalid Credentials..!!</p>
+    <button class="btn" type="submit" name="login-submit">Login </button>
+    <p>Not yet a member?</p>
+    <button class="b">Sign up</button>
+  </div>
+</form>
+</body>
+</html>
